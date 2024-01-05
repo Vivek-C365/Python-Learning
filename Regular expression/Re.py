56a4edb452b2f9a3a0b7f188458d8cff09b74fd6
Vivek Choudhary <vivek.choudhary.0022@gmail.com>
2024-01-05 16:50:05 +0530

    update

--- Regular expression/Re.py
+++
@@ -1,8 +0,0 @@
-import re
-text = """Contact us at support@example.com or sales@example.com
-You can reach us at +1234567890 or +1-987-654-3210
-9877108932
-"""
-phone_pattern=r'\d{10}'
-phone_numbers = re.findall(phone_pattern, text)
-print("Phone Numbers:", phone_numbers)
