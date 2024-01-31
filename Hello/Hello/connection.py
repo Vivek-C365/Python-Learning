6a0ba3c505f96fc0b9ea0405be621b031e48d066
Vivek Choudhary <95847089+Vivek-C365@users.noreply.github.com>
2024-01-31 15:07:49 +0530

    Delete Hello directory

--- Hello/Hello/connection.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-
-#establishing the connection
-conn = mysql.connector.connect(
-   user='root', password='admin', host='localhost', database='sample')
-
-cursor = conn.cursor()
